562903ed496020897a84cbefe400430163bf1471
Travis Riddle <noreply>
2023-10-22 10:02:02 -0600

    feat: Add tests for validation with disk, networking

--- tests/main.tf
+++ tests/main.tf
@@ -22,3 +22,49 @@ module "test_vm" {
     }
   }
 }
+
+module "test_vm_with_disk_networking" {
+  source = "../"
+
+  clone_ci = {
+    vm = {
+      name = "test"
+      target_pve_node = "pve_test"
+      clone = "test_clone"
+      disk_type         = "scsi"
+      disk_storage_pool = "local-lvm"
+      disk_size         = "60G"
+      network_model     = "virtio"
+      network_bridge    = "vmbr0"
+    }
+  }
+}
+
+module "test_vm_with_disk" {
+  source = "../"
+
+  clone_ci = {
+    vm = {
+      name = "test"
+      target_pve_node = "pve_test"
+      clone = "test_clone"
+      disk_type         = "scsi"
+      disk_storage_pool = "local-lvm"
+      disk_size         = "60G"
+    }
+  }
+}
+
+module "test_vm_with_networking" {
+  source = "../"
+
+  clone_ci = {
+    vm = {
+      name = "test"
+      target_pve_node = "pve_test"
+      clone = "test_clone"
+      network_model     = "virtio"
+      network_bridge    = "vmbr0"
+    }
+  }
+}
